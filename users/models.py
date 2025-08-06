574424f546bf35a26f65f8cc7eebfaae16e75416
Francis77v <floresfrancisjoseph28@gmail.com>
2025-08-06 15:02:11 +0800

    Initial commit

---
+++ users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+# from django.contrib.auth.models import AbstractUser
+# # Create your models here.
+#
+# class CustomUser(AbstractUser):
+#     ROLE_CHOICES = [
+#         ("admin", "Admin"),
+#         ("read", "Read"),
+#         ("write", "Write"),
+#     ]
+#
+#     role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+#
+#     def __str__(self):
+#         return f"Welcome {self.username}, You can only {self.role}"
+#
+#
